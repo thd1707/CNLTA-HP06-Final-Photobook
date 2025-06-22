1df358edcecc46839424159038ea0cbb6bb71d05
Hong Duc <tranhongduchkn@gmail.com>
2025-06-22 10:04:28 +0700

    Initial Commit

---
+++ CNLTA-HP06-Final-Photobook/CNLTA_HP06_Final_PhotobookApp.swift
@@ -0,0 +1,17 @@
+//
+//  CNLTA_HP06_Final_PhotobookApp.swift
+//  CNLTA-HP06-Final-Photobook
+//
+//  Created by TY on 22/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct CNLTA_HP06_Final_PhotobookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CNLTA-HP06-Final-Photobook/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CNLTA-HP06-Final-Photobook
+//
+//  Created by TY on 22/06/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
